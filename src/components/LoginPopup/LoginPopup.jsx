7f978197c2f17bf4b3fdf96027ada27e383ac072
Seth Akoto <sethakotodid@gmail.com>
2024-05-14 00:18:06 +0000

    updates

--- src/components/LoginPopup/LoginPopup.jsx
+++ src/components/LoginPopup/LoginPopup.jsx
@@ -13,11 +13,13 @@ const [currState , setCurrState ] = useState("Login")
             <h2>{currState}</h2>
             <img onClick={() => setShowLogin(false)} src={assets.cross_icon} alt="" />
         </div>
+        
         <div className="login-popup-inputs">
             {currState ==="Login" ? <></> :  <input type="text" placeholder='Your name' required/> }
             <input type="email" placeholder='Your email' required />
             <input type="password" placeholder='Password' required />
         </div>
+
         <button>{currState==="Sign Up" ? "Create account" : "Login"}</button>
         <div className="login-popup-condition">
             <input type="checkbox" required />
