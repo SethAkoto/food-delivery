b797cffe6da25ace78edeeb9d617d9fd56ca9189
Seth Akoto <sethakotodid@gmail.com>
2024-05-14 23:10:06 +0000

    updates

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,27 +6,55 @@ import { Link } from 'react-router-dom';
 const Navbar = ({setShowLogin}) => {
 
   const [menu , setMenu ] = useState("home");
-
+   
+  
   
   return (
-    <div className='navbar'>
-      <img src={assets.logo} alt="" className="logo" />
+    <div className="navbar">
+      <Link to='/'>
+        <img src={assets.logo} alt="" className="logo" />
+      </Link>
+
       <ul className="navbar-menu">
-        <Link to='/' onClick={() => setMenu("home")} className={menu==="home"?"active":""}>home</Link>
-        <Link to='/explore' onClick={() => setMenu("menu")} className={menu==="menu"?"active" : ""}>menu</Link>
-        <li onClick={() => setMenu("mobile-app")} className={menu==="mobile-app"?"active" : ""}>mobile-app</li>
-        <li onClick={() => setMenu("contact us")} className={menu==="contact us"?"active" : ""}>contact us</li>
+        <Link
+          to="/"
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          home
+        </Link>
+        <Link
+          to="/explore"
+          onClick={() => setMenu("menu")}
+          className={menu === "menu" ? "active" : ""}
+        >
+          menu
+        </Link>
+        <li
+          onClick={() => setMenu("mobile-app")}
+          className={menu === "mobile-app" ? "active" : ""}
+        >
+          mobile-app
+        </li>
+        <li
+          onClick={() => setMenu("contact us")}
+          className={menu === "contact us" ? "active" : ""}
+        >
+          contact us
+        </li>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
-          <img src={assets.basket_icon} alt="" />
+          <Link to="/cart">
+            <img src={assets.basket_icon} alt="" />
+          </Link>
           <div className="dot"></div>
         </div>
         <button onClick={() => setShowLogin(true)}>sign in</button>
       </div>
     </div>
-  )
+  );
 }
 
 export default Navbar
